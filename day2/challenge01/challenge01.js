357a916f392cd96ee229449f73015989fd6af99e
muratkilic1978 <muratkilic1978@gmail.com>
2019-02-04 07:26:01 +0100

    I've uploaded two JS challenges and one DOM-exercise
    I've uploaded two JS challenges and one DOM-exercise

---
+++ day2/challenge01/challenge01.js
@@ -0,0 +1,13 @@
+/***************************
+*  JS Code Challenge 01
+*/
+/*
+Mark and John are trying to compare their BMI (Body Mass index), which is calculated using the formula: BMI = mass / (height * height). Hint mass in kg and height in meter
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about wheter Mark has a higher BMI than John
+4. Print a string to the console containing the variable from step 3. (Someting like "Us Marks's BMI higher than John's? true").
+Good luck :-)
+*/
+
+
---
+++ day2/challenge02/challenge02.js
@@ -0,0 +1,12 @@
+/***************************
+*  JS Challenge 01
+*  
+*/
+/* 
+John and Mike both play basketball in different teams. In the latest 3 games, John team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score).
+GOOD LUCK :-)
+Tip: tofixed(2) rounds value to two decimal
+*/
