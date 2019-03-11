48307773f966373e75dbdf7ea14c20867710f8d1
muratkilic1978 <muratkilic1978@gmail.com>
2019-03-11 07:10:29 +0100

    I have uploaded one javascript event listener tutorial project

---
+++ day5/javascript-event-listeners-tutorial/js/script.js
@@ -0,0 +1,5 @@
+// JavaScript Events Listeners
+//Event-driven programmering: your program waits for events and uses them as triggers to run the next function(s)
+
+// Syntax
+// object.addEventListener(event, function)
